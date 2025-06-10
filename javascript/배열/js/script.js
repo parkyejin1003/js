5c32c1fbf29428b586ed50bdf76a59f4c7704342
Mr.OoO <avksek56@naver.com>
2025-06-10 17:41:32 +0900

    js

---
+++ javascript/배열/js/script.js
@@ -0,0 +1,21 @@
+/* let arr=[1,2,3,4,5]
+
+var a=arr.slice(1,4);
+
+console.log(a)
+ */
+
+var arr=[1,2,3,4,5]
+
+var d=arr.includes(26)
+console.log(d)
+
+let arr1=["나는", "지현", "입니다"];
+arr1.splice(1,1,"대한민국","국군")
+console.log(arr1);
+
+/* var b=arr.concat([3,6]);
+console.log(b);  */
+//var c=b.lastIndexOf(3); //indexOf는 찾고자 하는 숫자나 문자의 순번을 나타냄
+/* var c=b.indexOf(9); 
+console.log(c) */
